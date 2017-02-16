022d880b41a37b8ea9d45c4719ad9912e0509a2d
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-16 16:15:09 +0000

    understanding the lexical environment

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,23 +1,35 @@
-var a;
-console.log(a);
+// var a;
+// console.log(a);
+//
+// if (a === undefined) {
+//   console.log('a is undefined!')
+// } else {
+//   console.log('a is defined!')
+// }
+//
+// function b() {
+//   var myVar;
+//   console.log(myVar);
+// }
+//
+// function a() {
+//   var myVar = 2;
+//   console.log(myVar);
+//   b();
+// }
+//
+// var myVar = 1;
+// console.log(myVar);
+// a();
 
-if (a === undefined) {
-  console.log('a is undefined!')
-} else {
-  console.log('a is defined!')
-}
 
-function b() {
-  var myVar;
-  console.log(myVar);
-}
-
-function a() {
+function c() {
+  function d() {
+    console.log(myVar);
+  }
   var myVar = 2;
-  console.log(myVar);
-  b();
+  d();
 }
 
 var myVar = 1;
-console.log(myVar);
-a();
+c();
