00d34508b8b778679813929d9b5606dc2fb9e01d
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-16 16:15:33 +0000

    un-comments code

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,26 +1,26 @@
-// var a;
-// console.log(a);
-//
-// if (a === undefined) {
-//   console.log('a is undefined!')
-// } else {
-//   console.log('a is defined!')
-// }
-//
-// function b() {
-//   var myVar;
-//   console.log(myVar);
-// }
-//
-// function a() {
-//   var myVar = 2;
-//   console.log(myVar);
-//   b();
-// }
-//
-// var myVar = 1;
-// console.log(myVar);
-// a();
+var a;
+console.log(a);
+
+if (a === undefined) {
+  console.log('a is undefined!')
+} else {
+  console.log('a is defined!')
+}
+
+function b() {
+  var myVar;
+  console.log(myVar);
+}
+
+function a() {
+  var myVar = 2;
+  console.log(myVar);
+  b();
+}
+
+var myVar = 1;
+console.log(myVar);
+a();
 
 
 function c() {
