1650592898c0931b13700a878ad18e5843a2ccd3
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-03-07 17:21:53 +0000

    understanding functional programming

--- exercises/functional-programming.js
+++ exercises/functional-programming.js
@@ -16,3 +16,19 @@ var array2 = mapForEach(array1, function(item) {
 });
 
 console.log(array2);
+
+var checkPastLimit = function(limiter, item) {
+  return item > limiter;
+}
+
+var array3 = mapForEach(array1, checkPastLimit.bind(this, 1));
+console.log(array3);
+
+var checkPastLimitSimplified = function(limiter) {
+  return function(limiter, item) {
+    return item > limiter;
+  }.bind(this, limiter);
+};
+
+var array4 = mapForEach(array1, checkPastLimitSimplified(2));
+console.log(array4);
